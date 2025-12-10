15ca0691187c006a774efb34d4dbc39de22c18de
Usman Chaudhry <usmanch@umich.edu>
2025-12-10 11:39:57 -0500

    3. GROUP BY and ORDER BY Statements

---
+++ 3. GROUP BY and ORDER BY.sql
@@ -0,0 +1,17 @@
+-- GROUP BY
+
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+
+
+
+
+-- ORDER BY
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age 
+;
+
