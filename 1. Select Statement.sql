382d725b9c71df6c3df2f48bdd1bc57ea4edf844
Usman Chaudhry <usmanch@umich.edu>
2025-12-10 11:05:03 -0500

    1. Select Statement

---
+++ 1. Select Statement.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM employee_demographics;
+
+SELECT first_name, 
+last_name, 
+birth_date,
+age, 
+age + 10
+FROM employee_demographics; 
+
+SELECT DISTINCT first_name, gender
+FROM Parks_and_Recreation.employee_demographics; 
