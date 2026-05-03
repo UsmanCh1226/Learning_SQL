722ba0c69d2a7ae9a8297d52687ce7651dbe345f
Usman Chaudhry <usmanch@umich.edu>
2026-05-03 07:25:17 -0400

    2. Unions

---
+++ 2. Unions.sql
@@ -0,0 +1,21 @@
+-- Unions
+
+SELECT first_name, last_name
+FROM employee_demographics;
+UNION
+SELECT first_name, last_name
+FROM employee_salary; 
+
+SELECT first_name, last_name, 'Old Male' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Fem ale' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female' 
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name
+;
